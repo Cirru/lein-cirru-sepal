5f7a6e5c1a9a98f0351a93962e4f81605297a5d7
jiyinyiyong <jiyinyiyong@gmail.com>
2015-11-14 16:32:47 +0800

    update sepal 0.0.5; bump 0.0.9

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cirru/lein-sepal "0.0.8"
+(defproject cirru/lein-sepal "0.0.9"
   :description "Leiningen plugin for compiling Cirru Sepal into Clojure"
   :url "https://github.com/Cirru/lein-cirru-sepal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cirru/sepal "0.0.4"]
+  :dependencies [[cirru/sepal "0.0.5"]
                  [cirru/parser "0.0.3"]
                  [hawk "0.2.5"]]
   :eval-in-leiningen true
